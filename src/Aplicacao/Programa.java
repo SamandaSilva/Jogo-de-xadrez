362b0cb0e3a7589bf4bd701835d7948b1881be23
Samanda Silva <samandasilva99@gmail.com>
2023-01-25 01:53:24 -0300

    camada de xadrez e impressão do tabuleiro

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,9 +1,13 @@
 package Aplicacao;
 
+import xadrez.Partida_de_xadrez;
+
 public class Programa {
 
 	public static void main(String[] args) {
 
+		Partida_de_xadrez partida = new Partida_de_xadrez();
+		UI.printTabuleiro(partida.getpecas());
 		
 	}
 
